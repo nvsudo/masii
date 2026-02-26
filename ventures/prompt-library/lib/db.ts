37f62a3d73282a47390b502921d57f9b3f316ba1
nvsudo <nik@the2050labs.com>
2026-02-26 20:46:44 +0400

    Revert prompt-library commits (moved to behavr/web/)
    This reverts commits:
    - 03670f5 (Add README and STATUS docs)
    - 2d2e17d (Redesign: Space background, hidden ratings, etc.)
    
    Reason: Prompt library is Behavr Phase 1, belongs in /ventures/processing/behavr/web/, not standalone /ventures/prompt-library/

--- ventures/prompt-library/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg'
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // Fly.io internal network doesn't need SSL
-  ssl: false
-})
-
-export default pool
